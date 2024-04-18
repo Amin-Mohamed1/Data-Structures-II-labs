197b09f7db879ca3de527da793cba075111dabb4
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2024-04-19 00:09:11 +0200

    Created EnglishDictionary.java

---
+++ Lap2(src-tests)/src/EnglishDictionary.java
@@ -0,0 +1,24 @@
+public class EnglishDictionary {
+    public EnglishDictionary(String backendType) {
+    }
+
+    public boolean insert(String insertString) {
+        return false;
+    }
+
+    public boolean delete(String deleteString) {
+        return false;
+    }
+
+    public boolean search(String searchString) {
+        return false;
+    }
+
+    public int[] batchInsertFromArray(String[] array) {
+        return new int[0];
+    }
+
+    public int[] batchDeleteFromArray(String[] array) {
+        return new int[0];
+    }
+}
