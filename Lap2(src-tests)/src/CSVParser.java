718fe7bbac524e5e571c5faa8233c8566a9b0e34
Amen Mohamed <145727573+WorldisAmen@users.noreply.github.com>
2024-04-19 02:27:49 +0200

    adjusted parameters to arraylist

--- Lap2(src-tests)/src/CSVParser.java
+++ Lap2(src-tests)/src/CSVParser.java
@@ -2,11 +2,10 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 public class CSVParser {
-    public static List<String> parseCSVFromFile(String filePath) throws IOException {
-        List<String> strings = new ArrayList<>();
+    public static ArrayList<String> parseCSVFromFile(String filePath) throws IOException {
+        ArrayList<String> strings = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = br.readLine()) != null) {
