393b6bc9f5f784908d4e2a82aaa17eb01bc02dd4
Habiba Tarek <147947993+habibatarek26@users.noreply.github.com>
2024-04-19 13:25:07 +0200

    Update PerfectHashing.java

--- Lap2(src-tests)/src/PerfectHashing.java
+++ Lap2(src-tests)/src/PerfectHashing.java
@@ -2,4 +2,5 @@ public interface PerfectHashing<T> {
     public boolean insert(T key);
     public boolean search(T key);
     public boolean delete(T key);
+    public int getNumberOfRehashing();
 }
